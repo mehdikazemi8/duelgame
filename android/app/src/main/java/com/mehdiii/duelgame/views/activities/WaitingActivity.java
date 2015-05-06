e5500a77703b411c22b318b69c622cbe9ba7c035
mehdi-kazemi8 <mehdi.kazemi8@gmail.com>
2015-05-06 15:16:03 +0430

    Mute unMute works fine

--- android/app/src/main/java/com/mehdiii/duelgame/views/activities/WaitingActivity.java
+++ android/app/src/main/java/com/mehdiii/duelgame/views/activities/WaitingActivity.java
@@ -9,8 +9,6 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.BounceInterpolator;
 import android.widget.ImageView;
