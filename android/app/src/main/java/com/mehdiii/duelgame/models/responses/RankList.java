e84282d145c2d83a8023c9e6062c1146664bdde6
mehdi-kazemi8 <mehdi.kazemi8@gmail.com>
2015-05-12 15:23:46 +0430

    Rank Position in Ranklist is shown

--- android/app/src/main/java/com/mehdiii/duelgame/models/responses/RankList.java
+++ android/app/src/main/java/com/mehdiii/duelgame/models/responses/RankList.java
@@ -1,31 +1,39 @@
 package com.mehdiii.duelgame.models.responses;
 
 import com.google.gson.annotations.SerializedName;
-import com.mehdiii.duelgame.models.User;
+import com.mehdiii.duelgame.models.UserForRanklist;
 import com.mehdiii.duelgame.models.base.BaseModel;
 
 import java.util.List;
 
 public class RankList extends BaseModel {
     @SerializedName("top")
-    private List<User> top;
+    private List<UserForRanklist> top;
 
     @SerializedName("near")
-    private List<User> near;
+    private List<UserForRanklist> near;
 
-    public List<User> getTop() {
+    public List<UserForRanklist> getTop() {
+        if (top.size() != 0) {
+            top.get(0).setPlaceInRank(1);
+            for (int i = 1; i < top.size(); i++)
+                if (top.get(i).getScore() == top.get(i - 1).getScore())
+                    top.get(i).setPlaceInRank(top.get((i - 1)).getPlaceInRank());
+                else
+                    top.get(i).setPlaceInRank(i + 1);
+        }
         return top;
     }
 
-    public void setTop(List<User> top) {
+    public void setTop(List<UserForRanklist> top) {
         this.top = top;
     }
 
-    public List<User> getNear() {
+    public List<UserForRanklist> getNear() {
         return near;
     }
 
-    public void setNear(List<User> near) {
+    public void setNear(List<UserForRanklist> near) {
         this.near = near;
     }
 
