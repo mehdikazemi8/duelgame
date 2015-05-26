efa7f7f9d5b83026eabeaec70d350a81ef8077b5
Omid Najaee <platipino@gmail.com>
2015-05-26 18:33:23 +0430

    gcm for porting users

--- android/app/src/main/java/com/mehdiii/duelgame/views/activities/home/HomeActivity.java
+++ android/app/src/main/java/com/mehdiii/duelgame/views/activities/home/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.mehdiii.duelgame.views.activities.home;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -8,13 +10,16 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 
-import com.android.vending.billing.IInAppBillingService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.mehdiii.duelgame.DuelApp;
 import com.mehdiii.duelgame.R;
 import com.mehdiii.duelgame.managers.PurchaseManager;
 import com.mehdiii.duelgame.models.BuyNotification;
 import com.mehdiii.duelgame.models.ChangePage;
-import com.mehdiii.duelgame.models.events.OnSoundStateChanged;
-import com.mehdiii.duelgame.utils.ScoreHelper;
+import com.mehdiii.duelgame.models.SendGcmCode;
+import com.mehdiii.duelgame.models.base.CommandType;
 import com.mehdiii.duelgame.views.OnCompleteListener;
 import com.mehdiii.duelgame.views.activities.ParentActivity;
 import com.mehdiii.duelgame.views.activities.home.fragments.FlippableFragment;
@@ -27,8 +32,11 @@ import com.mehdiii.duelgame.views.custom.ToggleButton;
 import com.mehdiii.duelgame.views.dialogs.ConfirmDialog;
 import com.mehdiii.duelgame.views.dialogs.ScoresDialog;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HomeActivity extends ParentActivity {
     private static final int REQUEST_CODE_PURCHASE = 10001;
@@ -54,6 +62,11 @@ public class HomeActivity extends ParentActivity {
 
 //    public DuelMusicPlayer musicPlayer;
 
+    static GoogleCloudMessaging gcm;
+    static Context context;
+    static String regid;
+    static String SENDER_ID = "753066845572";
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (!PurchaseManager.getInstance().handleActivityResult(resultCode, data))
@@ -64,16 +77,91 @@ public class HomeActivity extends ParentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        context = getApplicationContext();
 
         find();
         configure();
 
         PurchaseManager.init(HomeActivity.this, REQUEST_CODE_PURCHASE);
+
+        // Check device for Play Services APK.
+        if (checkPlayServices()) {
+            // If this check succeeds, proceed with normal processing.
+            // Otherwise, prompt user to get valid Play Services APK.
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regid = DuelApp.getRegistrationId(context);
+
+            if (regid.isEmpty()) {
+                registerInBackground();
+            }
+        } else {
+            Log.i(TAG, "No valid Google Play Services APK found.");
+        }
+    }
+
+    public static void registerInBackground() {
+        registerInBackground(null);
+    }
+
+    public static void registerInBackground(final OnCompleteListener onComplete) {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+
+                    // You should send the registration ID to your server over HTTP,
+                    // so it can use GCM/HTTP or CCS to send messages to your app.
+                    // The request to your server should be authenticated if your app
+                    // is using accounts.
+                    sendRegistrationIdToBackend();
+
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+            }
+        }.execute(null, null, null);
+    }
+
+    public static void sendRegistrationIdToBackend() {
+        DuelApp.getInstance().sendMessage(new SendGcmCode(regid).serialize(CommandType.SEND_GCM_CODE));
+    }
+
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    public static final String TAG = "TAG_GCM";
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 
     ScoresDialog scoresDialog;
-    public void viewLevels(View view)
-    {
+
+    public void viewLevels(View view) {
         scoresDialog.show();
     }
 
@@ -138,7 +226,8 @@ public class HomeActivity extends ParentActivity {
 
     private ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
         @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
 
 
         @Override
