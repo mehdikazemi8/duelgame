5df440075228236d79eb413d86c96cac3dc41c41
MeHdi <mehdi.kazemi8@gmail.com>
2015-12-30 02:02:34 +0330

    HomeFragment changed

--- android/app/src/main/java/com/mehdiii/duelgame/views/activities/home/fragments/home/HomeFragment.java
+++ android/app/src/main/java/com/mehdiii/duelgame/views/activities/home/fragments/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,7 +30,6 @@ import com.mehdiii.duelgame.views.activities.category.CategoryActivity;
 import com.mehdiii.duelgame.views.activities.flashcards.FlashCardActivity;
 import com.mehdiii.duelgame.views.activities.home.fragments.FlippableFragment;
 import com.mehdiii.duelgame.views.dialogs.DuelDialog;
-import com.mehdiii.duelgame.views.dialogs.DuelFriendDialog;
 import com.mehdiii.duelgame.views.dialogs.HeartLowDialog;
 
 import de.greenrobot.event.EventBus;
@@ -42,26 +42,14 @@ public class HomeFragment extends FlippableFragment implements View.OnClickListe
     TextView diamondCount;
     ImageView avatarImageView;
     TextView titleTextView;
-//    TextView levelText;
-//    TextView totalRankingText;
-//    TextView totalRanking;
-//    TextView friendsRankingText;
-//    TextView friendsRanking;
-//    TextView provinceRankingText;
-//    TextView provinceRanking;
     TextView textViewCounter;
-//    TextView textViewSendReport;
-//    ImageButton addFriendButton;
-//    ImageView duelButton;
-//    ProgressBar levelProgress;
     Button refillButton;
     Button buyDiamondButton;
-    TextView flashCardButton;
-    TextView duel2Button;
+    Button flashCardButton;
+    Button duel2Button;
     TextView textViewHearts;
     ImageView heartsImageView;
     LinearLayout containerHearts;
-//    Button telegramBotButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -87,47 +75,34 @@ public class HomeFragment extends FlippableFragment implements View.OnClickListe
     }
 
     private void configure(View view) {
-//        addFriendButton.setOnClickListener(this);
         refillButton.setOnClickListener(this);
         buyDiamondButton.setOnClickListener(this);
-//        duelButton.setOnClickListener(this);
         flashCardButton.setOnClickListener(this);
         duel2Button.setOnClickListener(this);
-//        textViewSendReport.setOnClickListener(this);
-//        telegramBotButton.setOnClickListener(this);
 
         // set font-face
         FontHelper.setKoodakFor(view.getContext(), flashCardButton, duel2Button, textViewHearts, /*textViewSendReport,*/
                 diamondCount, titleTextView/*, levelText, totalRankingText,
                 totalRanking, friendsRankingText, friendsRanking,
-                provinceRanking, provinceRankingText*/, textViewCounter, buyDiamondButton, refillButton
+                provinceRanking, provinceRankingText*/, textViewCounter
         );
+
+        buyDiamondButton.setTypeface(FontHelper.getIcons(getActivity()));
+        refillButton.setTypeface(FontHelper.getIcons(getActivity()));
     }
 
     private void find(View view) {
         diamondCount = (TextView) view.findViewById(R.id.home_diamond_cnt);
         avatarImageView = (ImageView) view.findViewById(R.id.imageView_avatar);
         titleTextView = (TextView) view.findViewById(R.id.textView_title);
-//        levelText = (TextView) view.findViewById(R.id.home_level_text);
-//        totalRankingText = (TextView) view.findViewById(R.id.home_total_ranking_text);
-//        totalRanking = (TextView) view.findViewById(R.id.home_total_ranking);
-//        friendsRankingText = (TextView) view.findViewById(R.id.home_friends_ranking_text);
-//        friendsRanking = (TextView) view.findViewById(R.id.home_friends_ranking);
-//        provinceRanking = (TextView) view.findViewById(R.id.home_province_ranking);
-//        provinceRankingText = (TextView) view.findViewById(R.id.home_province_ranking_text);
-//        textViewSendReport = (TextView) view.findViewById(R.id.textView_send_report);
-//        addFriendButton = (ImageButton) view.findViewById(R.id.button_add_friend);
         refillButton = (Button) view.findViewById(R.id.button_refill);
         buyDiamondButton = (Button) view.findViewById(R.id.button_buy_diamond);
-//        levelProgress = (ProgressBar) view.findViewById(R.id.home_level_progress);
         textViewHearts = (TextView) view.findViewById(R.id.textView_heart);
         textViewCounter = (TextView) view.findViewById(R.id.textView_counter);
-//        duelButton = (ImageView) view.findViewById(R.id.button_duel);
         heartsImageView = (ImageView) view.findViewById(R.id.imageView_hearts);
         containerHearts = (LinearLayout) view.findViewById(R.id.container_hearts);
-        flashCardButton = (TextView) view.findViewById(R.id.button_flash_card);
-        duel2Button = (TextView) view.findViewById(R.id.button_duel2);
-//        telegramBotButton = (Button) view.findViewById(R.id.button_telegram_channel);
+        flashCardButton = (Button) view.findViewById(R.id.button_flash_card);
+        duel2Button = (Button) view.findViewById(R.id.button_duel2);
     }
 
     @Override
@@ -146,10 +121,6 @@ public class HomeFragment extends FlippableFragment implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-//            case R.id.button_telegram_channel:
-//                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://telegram.me/duelkonkoor"));
-//                startActivity(browserIntent);
-//                break;
             case R.id.button_add_friend:
                 break;
             case R.id.button_buy_diamond:
@@ -164,9 +135,6 @@ public class HomeFragment extends FlippableFragment implements View.OnClickListe
                 Intent intent = new Intent(getActivity(), FlashCardActivity.class);
                 startActivity(intent);
                 break;
-//            case R.id.textView_send_report:
-//                sendReport();
-//                break;
         }
     }
 
@@ -182,16 +150,7 @@ public class HomeFragment extends FlippableFragment implements View.OnClickListe
 
         avatarImageView.setImageResource(AvatarHelper.getResourceId(getActivity(), user.getAvatar()));
         diamondCount.setText(String.valueOf(user.getDiamond()));
-//        levelText.setText(String.valueOf(ScoreHelper.getLevel(user.getScore())));
-//        levelProgress.setProgress(ScoreHelper.getThisLevelPercentage(user.getScore()));
         titleTextView.setText(ScoreHelper.getTitle(user.getScore()));
-
-//        if (user.getRank() != null) {
-//            totalRanking.setText(String.valueOf(user.getRank().getTotal()));
-//            provinceRanking.setText(String.valueOf(user.getRank().getProvince()));
-//            friendsRanking.setText(String.valueOf(user.getRank().getFriends()));
-//        }
-//        provinceRankingText.setText(ProvinceManager.get(getActivity(), user.getProvince()));
 
         if (user.isExtremeHeart()) {
             heartsImageView.setImageResource(R.drawable.extreme_heart);
@@ -211,8 +170,6 @@ public class HomeFragment extends FlippableFragment implements View.OnClickListe
 
         DuelDialog duelDialog = new DuelDialog(getActivity());
         duelDialog.show();
-
-//        getActivity().startActivity(new Intent(getActivity(), CategoryActivity.class));
     }
 
     private void refillHeart() {
